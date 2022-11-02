4f8eac8c202e07aec850bf19a6e93c209aba8183
AlsuNuts <forever-nuts@yandex.ru>
2022-11-02 19:54:18 +0300

    Init commit

--- src/main/java/org/example/BaseRepository.java
+++ src/main/java/org/example/BaseRepository.java
@@ -4,7 +4,15 @@ package org.example;
 с CRUD операциями и выборкой всех объектов (для любого класса) findAll()
  */
 
+//import java.util.List;
+//import java.util.Optional;
+
 public interface BaseRepository <T>{
+    //я пока еще пытаюсь понять, нужно это или я усложняю, поэтому просто закомментирую следующие строчки
+    //Optional<T> get(int id);
+    //List<T> getAll();
+
+
     public T create(T item);
     public void read(T item);
     public void update(T item);
